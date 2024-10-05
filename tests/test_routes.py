7972414cefec16baf5969448e2c140c9f9809dc6
John M <jdmalcolm1602@gmail.com>
2024-10-05 14:55:16 -0400

    do Task 3c

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -198,13 +198,28 @@ class TestProductRoutes(TestCase):
         response = self.client.put(f"{BASE_URL}/0", json=new_product)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_delete_product(self):
+        """It should Delete a Product"""
+        products = self._create_products(5)
+        product_count = self.get_product_count()
+        test_product = products[0]
+        response = self.client.post(f"{BASE_URL}", json=test_product.serialize())
+        response = self.client.delete(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(response.data), 0)
+        # make sure they are deleted
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        new_count = self.get_product_count()
+        self.assertEqual(new_count, product_count - 1)
+
     ######################################################################
     # Utility functions
     ######################################################################
 
     def get_product_count(self):
         """save the current number of products"""
-        response = self.client.get(BASE_URL)
+        response = self.client.get(f"{BASE_URL}")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         # logging.debug("data = %s", data)
